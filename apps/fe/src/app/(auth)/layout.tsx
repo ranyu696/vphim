8e9dd9245607c3d4cd22e7b968e1ae61e1e1eb77
lehuygiang28 <lehuygiang28@gmail.com>
2025-04-30 16:41:22 +0700

    refactor(fe/auth): 🛠️ update authentication layout to streamline sign-out process
    - Commented out the useRouter import and related router logic in the RequiredAuthLayout component.
    - Updated signOut to use redirect and callbackUrl for improved navigation after sign-out.
    - Aims to enhance clarity and maintainability of the authentication flow.

--- apps/fe/src/app/(auth)/layout.tsx
+++ apps/fe/src/app/(auth)/layout.tsx
@@ -2,17 +2,16 @@
 
 import { PropsWithChildren } from 'react';
 import { useIsAuthenticated } from '@refinedev/core';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 import { signOut } from 'next-auth/react';
 import '@/components/layout/layout.css';
 
 export default function RequiredAuthLayout({ children }: PropsWithChildren) {
-    const router = useRouter();
+    // const router = useRouter();
     const { data, isLoading } = useIsAuthenticated();
 
     if (!isLoading && !data?.authenticated) {
-        signOut();
-        router.replace('/');
+        signOut({ redirect: true, callbackUrl: '/' });
         return <></>;
     }
 
